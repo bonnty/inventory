6b3a65b47b67798415896bcad91444ba88d3186a
bonnty <43603875+bonnty@users.noreply.github.com>
2025-01-30 12:10:05 +0100

    feat: rating from 1 to 100

--- Models/BoardGame.cs
+++ Models/BoardGame.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Inventory.Models {
     public class BoardGame
     {
 	    public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+
+        [Range(0, 100)]
         public int Rating { get; set; }
         public bool IsDone { get; set; }
     }
