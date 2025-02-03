44a9570c8d0bd4a659cc47c8fd59c3a3096d97cb
bonnty <43603875+bonnty@users.noreply.github.com>
2025-02-03 14:23:33 +0100

    feat: trying fluent, better since validation are handle based on the model (itself link to db) not on the page

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 namespace Inventory.Models
@@ -13,5 +14,37 @@ namespace Inventory.Models
 
         [Range(0, 10)]
         public int? Rating { get; set; }
+    }
+
+    public class BookModelFluentValidator : AbstractValidator<Book>
+    {
+        public BookModelFluentValidator()
+        {
+            RuleFor(x => x.Isbn)
+                .NotEmpty();
+
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .Length(1,200);
+
+            RuleFor(x => x.Author)
+                .NotEmpty()
+                .Length(1,200);
+            
+            RuleFor(x => x.Summary)
+                .NotEmpty()
+                .Length(0,200);
+
+            RuleFor(x => x.Rating)
+                .NotEmpty();
+        }
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<Book>.CreateWithOptions((Book)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
     }
 }
